57e50172428850ae8fa598f0c32794e19f3622c5
David <kansas@inkwhiskey.com>
2017-06-19 16:04:30 -0700

    carousel updates 2

--- src/_js/script.js
+++ src/_js/script.js
@@ -3,5 +3,18 @@ $('.project').slick({
   infinite: true,
   speed: 200,
   slidesToShow: 1,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        dots: true,
+			  infinite: true,
+			  speed: 200,
+			  slidesToShow: 1,
+			  slidesToScroll: 1,
+			  arrows: false
+      }
+    }
+  ]
 });
