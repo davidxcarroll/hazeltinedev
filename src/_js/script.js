202dfb8438120659bc4c9efcc2359b42bc5af08a
David <kansas@inkwhiskey.com>
2017-06-22 09:16:36 -0700

    added services and timeline

--- src/_js/script.js
+++ src/_js/script.js
@@ -43,3 +43,33 @@ $('.testimonials-list').slick({
     }
   ]
 });
+
+// ABOUT ACCORDIONS
+
+$('#about-timeline-button').click(function(){
+  $('.about-timeline').slideToggle(300);
+  if($('#about-timeline-button .emoji').hasClass('emoji-arrow-down')){
+   $('#about-timeline-button .emoji').removeClass('emoji-arrow-down').addClass('emoji-arrow-up');
+  } else if($('#about-timeline-button .emoji').hasClass('emoji-arrow-up')){
+   $('#about-timeline-button .emoji').removeClass('emoji-arrow-up').addClass('emoji-arrow-down');
+  }
+});
+
+$('#about-services-button').click(function(){
+  $('.about-services').slideToggle(300);
+  if($('#about-services-button .emoji').hasClass('emoji-arrow-down')){
+   $('#about-services-button .emoji').removeClass('emoji-arrow-down').addClass('emoji-arrow-up');
+  } else if($('#about-services-button .emoji').hasClass('emoji-arrow-up')){
+   $('#about-services-button .emoji').removeClass('emoji-arrow-up').addClass('emoji-arrow-down');
+  }
+});
+
+// GOOGLE ANALYTICS
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-101431937-1', 'auto');
+ga('send', 'pageview');
